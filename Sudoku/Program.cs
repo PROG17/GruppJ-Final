964d9e569764f60f91594430491896427643039f
Joakim Fröjd <frojdie@gmail.com>
2017-09-19 10:29:54 +0200

    Initial code, program.cs

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -10,6 +10,10 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
+            var game = new Sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+            game.CreateBoard();
+            game.Solve();
+            Console.ReadKey();
         }
     }
 }
