2a136a803ca32798f4bcdef6d774e62e472e7589
Gustav Cleveman <31858743+GustavCleveman@users.noreply.github.com>
2017-09-19 10:45:29 +0200

    Build board och displayboard

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -11,8 +11,7 @@ namespace Sudoku
         static void Main(string[] args)
         {
             var game = new Sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
-            game.CreateBoard();
-            game.Solve();
+            //game.Solve();
             Console.ReadKey();
         }
     }
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Sudoku
+    {
+       //fields 
+        public int[,] board = new int [9, 9];
+        public string number;
+        public int index = 0;
+
+        //constructor
+        public Sudoku (string num)
+        {
+            number = num;
+            CreateBoard();
+        }
+
+        //metoder
+        public void CreateBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    board[i, j] = (int) Char.GetNumericValue(number[index]);
+                    index++;
+                }
+            }
+            DisplayBoard();
+        }
+
+        public void DisplayBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(board[i,j]);
+                    if (j == 2 || j == 5)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+                if (i == 2 || i == 5)
+                {
+                    Console.WriteLine("-----------");
+                }
+                
+            }
+        }
+
+
+
+    }
+}
