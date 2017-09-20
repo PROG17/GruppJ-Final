03597c75877a15d6538db34a371fb9a90c179236
Gustav Cleveman <31858743+GustavCleveman@users.noreply.github.com>
2017-09-20 13:57:31 +0200

    påbörjat solvekod

--- SudokuHard/Program.cs
+++ SudokuHard/Program.cs
@@ -10,7 +10,7 @@ namespace SudokuHard
     {
         static void Main(string[] args)
         {
-            var game = new Sudoku("");
+            var game = new Sudoku("037060000205000800006908000000600024001503600650009000000302700009000402000050360");
 
         }
     }
--- SudokuHard/Sudoku.cs
+++ SudokuHard/Sudoku.cs
@@ -14,14 +14,16 @@ namespace SudokuHard
         private string nums = "";
         private int index = 0;
         int[,] board = new int[9,9];
-
+        private bool solved = false;
 
         //constructor
         public Sudoku(string num)
         {
             nums = num;
+            BoardBuilder();
         }
 
+        //metoder
         private void BoardBuilder()
         {
             for (int i = 0; i < 9; i++)
@@ -30,6 +32,43 @@ namespace SudokuHard
                 {
                     board[i, j] = (int) Char.GetNumericValue(nums[index]);
                     index++;
+                }
+            }
+            DisplayBoard();
+        }
+
+        private void DisplayBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(board[i,j]);
+                    if (j == 2 || j == 5)
+                    {
+                        Console.Write("|");
+                    }
+                    
+                }
+                Console.WriteLine();
+                if (i == 2 || i == 5)
+                {
+                    Console.WriteLine("-------------");
+                }
+
+            }
+        }
+
+        public void solve()
+        {
+            while (!solved)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        
+                    }
                 }
             }
         }
