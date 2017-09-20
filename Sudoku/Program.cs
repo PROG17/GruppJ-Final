5cc80fc85c1ffd34b669fe6ce32dc5666397d219
Gustav Cleveman <31858743+GustavCleveman@users.noreply.github.com>
2017-09-20 13:38:33 +0200

    SudokuSvår

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -10,9 +10,11 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            var game = new Sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+            //var game = new Sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+            var game = new Sudoku("401290075200300800070080006000103062105000403730608000600020030007001004890065107");
             game.Solve();
             Console.ReadKey();
+            Console.WriteLine(game.ToString());
         }
     }
 }
