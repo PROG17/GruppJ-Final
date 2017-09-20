f70621b8bce545343ae11b668abe461de3048a23
Gustav Cleveman <31858743+GustavCleveman@users.noreply.github.com>
2017-09-20 13:47:43 +0200

    class och brädeskapat

--- SudokuHard/Program.cs
+++ SudokuHard/Program.cs
@@ -10,6 +10,8 @@ namespace SudokuHard
     {
         static void Main(string[] args)
         {
+            var game = new Sudoku("");
+
         }
     }
 }
---
+++ SudokuHard/Sudoku.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuHard
+{
+    class Sudoku
+    {
+        //fields
+        private string nums = "";
+        private int index = 0;
+        int[,] board = new int[9,9];
+
+
+        //constructor
+        public Sudoku(string num)
+        {
+            nums = num;
+        }
+
+        private void BoardBuilder()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    board[i, j] = (int) Char.GetNumericValue(nums[index]);
+                    index++;
+                }
+            }
+        }
+
+    }
+}
