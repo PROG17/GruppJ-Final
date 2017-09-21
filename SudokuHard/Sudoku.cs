5cfbf478ecbb580bd2c3344caf373da216b6da70
Joakim Fröjd <frojdie@gmail.com>
2017-09-21 11:17:15 +0200

    Nästan löst. Fåtal nollor kvar.... Inga dubletter i rader, kolumner eller boxar

--- SudokuHard/Sudoku.cs
+++ SudokuHard/Sudoku.cs
@@ -14,7 +14,8 @@ namespace SudokuHard
         //fields
         private string nums = "";
         private int index = 0;
-        int[,] board = new int[9,9];
+        static int[,] board = new int[9, 9];
+        int[,] clone = board;
         private bool solved = false;
         //List<int> numbersPrintedList = new List<int>(); //Lista att spara ner färdiga värden i cellens rad, kolumn och box
 
@@ -32,7 +33,7 @@ namespace SudokuHard
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    board[i, j] = (int) Char.GetNumericValue(nums[index]);
+                    board[i, j] = (int)Char.GetNumericValue(nums[index]);
                     index++;
                 }
             }
@@ -45,12 +46,12 @@ namespace SudokuHard
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    Console.Write(board[i,j]);
+                    Console.Write(board[i, j]);
                     if (j == 2 || j == 5)
                     {
                         Console.Write("|");
                     }
-                    
+
                 }
                 Console.WriteLine();
                 if (i == 2 || i == 5)
@@ -61,57 +62,80 @@ namespace SudokuHard
             }
         }//Skriver ut spelbrädet
 
-        public void solve()
+        public void Solve()
         {
-            while (!solved) //Lopp som upprepas till pusslet är löst
+
+            do
             {
-                for (int i = 0; i < 9; i++)
+                Search(clone);
+            } while (false);
+            Console.WriteLine("Solved\n");
+            DisplayBoard();
+        }
+        public bool Search(int[,] array)
+        {
+            bool solved = true;
+            //while (!solved) //Lopp som upprepas till pusslet är löst
+            //{
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
                 {
-                    for (int j = 0; j < 9; j++)
+                    var temp = array[i, j]; //Variabel får värdet i aktuell cell
+                    if (temp == 0)//Om värdet är 0
                     {
-                        var temp = board[i, j]; //Variabel får värdet i aktuell cell
-                        if (temp == 0)//Om värdet är 0
+                        solved = false;
+                        List<int> numbersPrintedList = new List<int>(); //Lista att spara ner färdiga värden i cellens rad, kolumn och box
+                        List<int> validNumbers = new List<int>(); //Lista för möjliga siffror
+                        numbersPrintedList = GetPrintedNumbers(i, j); //Hämta in färdiga värden som inte kan skrivas ut
+                        for (int k = 1; k <= 9; k++) //fyll listan med siffror 1-9
                         {
-                            List<int> numbersPrintedList = new List<int>(); //Lista att spara ner färdiga värden i cellens rad, kolumn och box
-                            List<int> validNumbers = new List<int>(); //Lista för möjliga siffror
-                            numbersPrintedList = GetPrintedNumbers(i, j); //Hämta in färdiga värden som inte kan skrivas ut
-                            for (int k = 1; k <= 9; k++) //fyll listan med siffror 1-9
+                            validNumbers.Add(k);
+                        }
+                        foreach (var item in numbersPrintedList) //Plocka bort siffror som redan finns i cellens "fält"
+                        {
+                            validNumbers.Remove(item);
+                        } //Nu innehåller listan de möjliga värdena för cellen.
+                        foreach (var item in validNumbers)
+                        {
+                            array[i, j] = item;
+
+                            if (CheckMove(i, j, item, array) == true)
                             {
-                                validNumbers.Add(k);   
+                                if (Search(array) == true)
+                                {
+                                    clone[i, j] = array[i, j];
+                                    return true;
+                                }
                             }
-                            foreach (var item in numbersPrintedList) //Plocka bort siffror som redan finns i cellens "fält"
-                            {
-                                validNumbers.Remove(item); 
-                            }
-
-
-
-
-
-
-                            Console.WriteLine("Nr upptagna från cell");
-                            foreach (var item in numbersPrintedList)////Skriver ut redan existerande värden (Endast kontroll)
-                            {
-                                Console.WriteLine(item);
-                            }
-                            Console.WriteLine("\nMöjliga nr");
-                            foreach (var item in validNumbers)////Skriver ut redan existerande värden (Endast kontroll)
-                            {
-                                Console.WriteLine(item);
-                            }
-                            Console.ReadKey();
 
                         }
                         
+                        //Console.WriteLine("Nr upptagna från cell");
+                        //foreach (var item in numbersPrintedList)////Skriver ut redan existerande värden (Endast kontroll)
+                        //{
+                        //    Console.WriteLine(item);
+                        //}
+                        //Console.WriteLine("\nMöjliga nr");
+                        //foreach (var item in validNumbers)////Skriver ut redan existerande värden (Endast kontroll)
+                        //{
+                        //    Console.WriteLine(item);
+                        //}
+                        //Console.ReadKey();
+
                     }
+
                 }
             }
+            if (solved == false) return false;
+            else return true;
+            //}
         }
 
 
         private List<int> GetPrintedNumbers(int row, int col) //Hämtar siffror i cellens rad, kol och box
         {
-            
+
             List<int> numbersFound = new List<int>();
 
             for (int j = 0; j < 9; j++)
@@ -149,9 +173,45 @@ namespace SudokuHard
             }
             numbersFound = numbersFound.Distinct().ToList();
             return numbersFound;
-            
+
 
 
         }
+        static bool CheckRow(int row, int val, int[,] arr)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (arr[row, i] == val) return false;
+            }
+            return true;
+        }
+        static bool CheckCol(int col, int val, int[,] arr)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (arr[col, i] == val) return false;
+            }
+            return true;
+        }
+        static bool CheckBox(int row, int col, int val, int[,] arr)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr[row - (row % 3) + i, col - (col % 3) + j] == val) return false;
+                }
+            }
+            return true;
+        }
+        static bool CheckMove(int row, int col, int val, int[,] arr)
+        {
+            if (!CheckRow(row, val, arr)) return false;
+            if (!CheckCol(col, val, arr)) return false;
+            if (!CheckBox(row, col, val, arr)) return false;
+            return true;
+
+        }
+
     }
 }
