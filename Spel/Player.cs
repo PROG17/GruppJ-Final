178bcb7ed3e5ddf553f32f30783c5b9080d888e8
Gustav Cleveman <31858743+GustavCleveman@users.noreply.github.com>
2017-09-22 13:31:01 +0200

    Klasser skapade

---
+++ Spel/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel
+{
+    class Player
+    {
+        private string name = "";
+        public List<Item> PlayerInventory = new List<Item>();
+
+        public Player(string _name)
+        {
+            name = _name;
+        }
+    }
+}
